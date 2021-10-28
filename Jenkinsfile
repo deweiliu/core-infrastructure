b6e91f89458af6710dae90b9a85e5d9a54fcf955
Dewei Liu <42348814+deweiliu@users.noreply.github.com>
2021-10-28 17:46:56 +0100

    Add Jenkinsfile

---
+++ Jenkinsfile
@@ -0,0 +1,23 @@
+pipeline {
+    agent {
+        label 'deployer-slave-amazon-linux'
+    }
+    
+    options {
+        timeout(time: 90, unit: 'MINUTES')
+    }
+
+    stages {
+        stage('Install CDK Packages'){
+            steps{
+                dir('cdk'){ sh 'npm install'}
+            }
+        }
+    }        
+    stage('CDK Deploy'){
+            steps{
+                dir('cdk'){ sh 'npm run cdk-deploy'}
+            }
+        }
+    }
+}
--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
-import { CdkStack } from '../lib/cdk-stack';
+import { CdkStack } from '../lib/core-infrastructure';
 
 const app = new cdk.App();
-new CdkStack(app, 'CdkStack', {
+new CdkStack(app, 'CoreInfrastructure', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
